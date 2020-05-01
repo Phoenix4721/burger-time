29e09d68f85a3a4ef71449d4397b5a4cd4e190b8
Chronos <jjboswell00@gmail.com>
2020-05-01 16:33:19 -0400

    -

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const routes = require('./controllers/burgers-controller')
+
+
+PORT = process.env.PORT || 8080
+
+const app = express()
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(routes)
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+
+app.listen(PORT, () => {
+    console.log('http://localhost:' + PORT)
+});
